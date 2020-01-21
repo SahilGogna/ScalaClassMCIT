c549506b03599b6fae1d170815cadd281aa8d5ce
SahilGogna <gogna.sahil95@gmail.com>
2020-01-20 20:58:46 -0500

    Basic File Reading

--- src/ca/mcit/scala/collections/FilePractice.scala
+++ src/ca/mcit/scala/collections/FilePractice.scala
@@ -1,8 +1,18 @@
 package ca.mcit.scala.collections
 
+import scala.io.{BufferedSource, Source}
+
 object FilePractice extends App {
 
-  val filepath = "/home/"
+  val filepath = "/home/bd-user/Documents/ScalaClassMCIT/person.csv"
+  val source: BufferedSource = Source.fromFile(filepath)
+  val input = source.getLines()
+      .map(line => line.split(","))
+      .map(a => CsvPerson(a(0).toInt,a(1),a(2)))
+      .foreach(person => println(person))
+
+
+  source.close()
 
 }
 
